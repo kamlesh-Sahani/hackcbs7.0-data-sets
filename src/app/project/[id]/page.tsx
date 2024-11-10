efba1aedac1c4e394583c2c55064731a3f5f7914
Sam <sambhavchugh2004@gmail.com>
2024-11-10 08:44:51 +0530

    project

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,22 +1,11 @@
-"use client";
-import React, { useState, useEffect } from 'react';
-import { FaFolder } from "react-icons/fa6";
-import ReactMarkdown from 'react-markdown';
-// FileCard Component
-import { auth  } from '@/auth';
-import { redirect } from 'next/navigation';
+import React from 'react';
 import RepositoryMenu from '@/components/projects/ProfileId';
-const ProfileIdPage =async ({ file }: any) => {
-    const session=await auth();
-    if (!session) {
-      redirect("/login");
-      return null;
-    }
-    return (
-       <>
-       <RepositoryMenu/>
-       </>
-    );
+const ProfileIdPage = async ({ file }: any) => {
+  return (
+    <>
+      <RepositoryMenu />
+    </>
+  );
 };
 
 export default ProfileIdPage;
